ac63bd6b9b429d41886758134bf1c54f2e119df9
KennyKoyama <kennysk1982@gmail.com>
2022-09-21 17:33:00 -0300

    Define a constant perimeter, sum all constants, console log perimeter

--- trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia01JSPrimeirosPassos/exercicio03.js
+++ trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia01JSPrimeirosPassos/exercicio03.js
@@ -3,3 +3,7 @@ const height = 8;
 const area = base * height;
 
 console.log(`base ${base} x height ${height} = ${area}`)
+
+const perimeter = base + height + area;
+
+console.log(`O perímetro do retângulo é ${perimeter}.`)
