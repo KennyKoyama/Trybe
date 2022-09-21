88ef8950df54eabcfa4abaf6daace50036a785b0
KennyKoyama <kennysk1982@gmail.com>
2022-09-21 15:48:54 -0300

    Calculate de diff between 2 numbers and sum with my birth year, console them

--- trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia01JSPrimeirosPassos/exercicio01.js
+++ trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia01JSPrimeirosPassos/exercicio01.js
@@ -3,3 +3,8 @@ const birthCity = 'Rio de Janeiro';
 let birthYear = 1982;
 
 console.log(`Me chamo ${myName}, nasci em ${birthYear} na cidade do ${birthCity}.`)
+
+let diff = 2030 - birthYear;
+birthYear += diff;
+
+console.log(`Agora eu nasci no ano de ${birthYear}, sim, eu vim do futuro! lol`)
