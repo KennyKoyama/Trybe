de665cde5399b618e5a6e0836ea085803779a8a9
KennyKoyama <kennysk1982@gmail.com>
2022-09-21 16:16:09 -0300

    Console log the type of patientAge but the variable is not declared

--- trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia01JSPrimeirosPassos/exercicio02.js
+++ trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia01JSPrimeirosPassos/exercicio02.js
@@ -10,3 +10,6 @@ console.log(`A variável patientId é do tipo primitivo ${typeof patientId}`)
 console.log(`A variável isEnrolled é do tipo primitivo ${typeof isEnrolled}`)
 console.log(`A variável patientInfo é do tipo primitivo ${typeof patientInfo}`)
 console.log(`A variável patientEmail é do tipo primitivo ${typeof patientEmail}`)
+
+console.log(`A variável patientAge é do tipo primitivo ${typeof patientAge} pois ela não foi declarada`)
+
