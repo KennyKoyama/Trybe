c8f2ad0d12ecca42231532f15925e900e26b0282
KennyKoyama <kennysk1982@gmail.com>
2022-09-21 20:37:56 -0300

    Solved using if/else and || operator

--- trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia01JSPrimeirosPassos/exercicio05.js
+++ trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia01JSPrimeirosPassos/exercicio05.js
@@ -6,7 +6,7 @@ if (typeof currentHour != 'number') return `Value ${currentHour} is invalid!`;
 if (currentHour >= 22) {
     message = `It's ${currentHour} o'clock. We shouldn't eat anything, it's time to sleep.`;
 } else if (currentHour >= 18 && currentHour < 22) {
-    message = `It's ${currentHour} o'clock. Chow time! Let's have dinner!`;
+    message = `It's ${currentHour} o'clock. Chow time! Let's have dinner! :D`;
 } else if (currentHour >= 14 && currentHour < 18) {
     message = `It's ${currentHour} o'clock. Shall we bake a cake for afternoon coffee?`;
 } else if (currentHour >= 11 && currentHour < 14) {
@@ -15,6 +15,15 @@ if (currentHour >= 22) {
     message = `It's ${currentHour} o'clock. Hmmm, smell of freshly brewed coffee!`;
 } else {
     message = `It's ${currentHour} o'clock. You should be sleeping!`;
-}
+};
 
-console.log(message)
+console.log(message);
+
+
+let weekDay = 'Wednesday';
+
+if (weekDay === 'Monday' || weekDay === 'Tuesday' || weekDay === 'Wednesday' || weekDay === 'Thursday' || weekDay === 'Friday') {
+    console.log('Yay, another day of learning at Trybe! >:D');
+} else {
+    console.log('FINALLY, well deserved rest!');
+};
