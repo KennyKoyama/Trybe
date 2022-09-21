597ebd6fb08c9ced330ed46bd8da2f68c28ed1da
KennyKoyama <kennysk1982@gmail.com>
2022-09-21 16:11:41 -0300

    Console log the type of the constants and variables

--- trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia01JSPrimeirosPassos/exercicio02.js
+++ trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia01JSPrimeirosPassos/exercicio02.js
@@ -5,3 +5,8 @@ const patientInfo = {
   lastName: 'Santos',
 };
 const patientEmail = 'ana@email.com';
+
+console.log(`A variável patientId é do tipo primitivo ${typeof patientId}`)
+console.log(`A variável isEnrolled é do tipo primitivo ${typeof isEnrolled}`)
+console.log(`A variável patientInfo é do tipo primitivo ${typeof patientInfo}`)
+console.log(`A variável patientEmail é do tipo primitivo ${typeof patientEmail}`)
