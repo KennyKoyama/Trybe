a3e504d96a17e8cc97bba61034d8bbf2286cf84b
KennyKoyama <kennysk1982@gmail.com>
2022-09-22 15:58:39 -0300

    Find index of a value on array

--- trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia02JSArrayELoopFor/arrays.js
+++ trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia02JSArrayELoopFor/arrays.js
@@ -3,3 +3,10 @@ let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let menuServices = menu[menu.indexOf('Serviços')];
 
 console.log(menuServices);
+
+
+// Procure o índice do valor “Portfólio” do array menu:
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
