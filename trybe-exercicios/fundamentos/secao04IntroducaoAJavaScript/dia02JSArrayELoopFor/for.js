89140339625fda9dc71774c6dbe05013ce667031
KennyKoyama <kennysk1982@gmail.com>
2022-09-22 16:08:14 -0300

    Print all items using loop for and console log

---
+++ trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia02JSArrayELoopFor/for.js
@@ -0,0 +1,6 @@
+// Utilize o for para imprimir os elementos da lista groceryList com o console.log()
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+for (let i = 0, size = groceryList.length; i < size; i += 1) {
+
+    console.log(groceryList[i])
+}
