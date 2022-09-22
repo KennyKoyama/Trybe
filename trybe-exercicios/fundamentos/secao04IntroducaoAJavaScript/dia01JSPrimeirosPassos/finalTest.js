5fa4f370aaa57b0f9e916b07a00782f531ce8eb2
KennyKoyama <kennysk1982@gmail.com>
2022-09-21 22:59:39 -0300

    Convert score using if/else, console log and return the result

--- trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia01JSPrimeirosPassos/finalTest.js
+++ trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia01JSPrimeirosPassos/finalTest.js
@@ -67,4 +67,27 @@ switch (chessPieceLowerCase) {
     default:
         console.log(`${chessPiece} is not a valid chess piece`);
         break;
-}
+};
+
+
+let score = Math.floor(Math.random() * 100);
+if (score < 0 || score > 100 || typeof score != 'number') return `${score} in invalid`;
+if (score >= 90) {
+    console.log('A');
+    return 'A'
+} else if (score >= 80 && score < 90) {
+    console.log('B');
+    return 'B'
+} else if (score >= 70 && score < 80) {
+    console.log('C');
+    return 'C'
+} else if (score >= 60 && score < 70) {
+    console.log('D');
+    return 'D'
+} else if (score >= 50 && score < 60) {
+    console.log('E');
+    return 'E'
+} else {
+    console.log('F');
+    return 'F'
+};
