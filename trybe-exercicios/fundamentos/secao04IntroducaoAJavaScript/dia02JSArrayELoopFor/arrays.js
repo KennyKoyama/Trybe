bc7a2753fa632e079fd17b49725e925fc6724593
KennyKoyama <kennysk1982@gmail.com>
2022-09-22 15:56:22 -0300

    Get one value from array

---
+++ trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia02JSArrayELoopFor/arrays.js
@@ -0,0 +1,5 @@
+// Obtenha o valor “Serviços” do array menu:
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[menu.indexOf('Serviços')];
+
+console.log(menuServices);
