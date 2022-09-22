88f1892739b19ec6b6771650e814f92dde746d52
KennyKoyama <kennysk1982@gmail.com>
2022-09-22 16:14:51 -0300

    Print all items using loop 'for/of'  and console log

---
+++ trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia02JSArrayELoopFor/forOf.js
@@ -0,0 +1,5 @@
+// Utilize o for/of para imprimir os elementos da lista names com o console.log()
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (const name of names) {
+    console.log(name)
+}
