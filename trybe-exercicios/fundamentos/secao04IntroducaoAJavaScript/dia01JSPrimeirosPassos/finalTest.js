74c5f908e4299a8d43d31ecec0f3b391682cb2e4
KennyKoyama <kennysk1982@gmail.com>
2022-09-21 21:27:42 -0300

    Find the greater between 2 and 3 values

--- trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia01JSPrimeirosPassos/finalTest.js
+++ trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia01JSPrimeirosPassos/finalTest.js
@@ -1,5 +1,6 @@
 let a = Math.floor(Math.random() * 10);
 let b = Math.floor(Math.random() * 10);
+let c = Math.floor(Math.random() * 10);
 
 const sum = a + b;
 const subtraction = a - b;
@@ -8,3 +9,5 @@ const division = a / b;
 const remainder = a % b;
 const exponent = a ** b;
 
+const greater = a > b ? a : b;
+const greaterBetween3 = greater > c ? greater : c;
