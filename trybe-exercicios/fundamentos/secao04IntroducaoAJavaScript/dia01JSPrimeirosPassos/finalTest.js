4d8b360b5dd7f0b0b9dffab3d6e0acf6b481affc
KennyKoyama <kennysk1982@gmail.com>
2022-09-21 21:21:03 -0300

    Make a constant for each arithmetic operation

---
+++ trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia01JSPrimeirosPassos/finalTest.js
@@ -0,0 +1,10 @@
+let a = Math.floor(Math.random() * 10);
+let b = Math.floor(Math.random() * 10);
+
+const sum = a + b;
+const subtraction = a - b;
+const multiplication = a * b;
+const division = a / b;
+const remainder = a % b;
+const exponent = a ** b;
+
