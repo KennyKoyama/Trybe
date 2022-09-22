7deaf692e2a915509c554ee772238312d14fdbb2
KennyKoyama <kennysk1982@gmail.com>
2022-09-22 19:09:13 -0300

    Reverse the sorted items

--- trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia02JSArrayELoopFor/exerciseBonus.js
+++ trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia02JSArrayELoopFor/exerciseBonus.js
@@ -4,3 +4,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sorted = numbers.sort((firstNumber, secondNumber) => firstNumber - secondNumber);
 console.log(sorted);
 
+// Ordene o array numbers em ordem decrescente e imprima seus valores
+let reverse = [...sorted].reverse();
+console.log(reverse)
+
