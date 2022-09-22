86ce7d5b35433de9c20ed1921b9069ac65fcf7ae
KennyKoyama <kennysk1982@gmail.com>
2022-09-22 18:26:50 -0300

    Print the average

--- trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia02JSArrayELoopFor/exercise.js
+++ trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia02JSArrayELoopFor/exercise.js
@@ -5,11 +5,14 @@ for (const number of numbers) {
     console.log(number);
 };
 
+
 // Some todos os valores contidos no array e imprima o resultado;
 console.log(numbers.reduce((sum,current) => sum + current, 0));
 
+
 // Calcule e imprima a média aritmética dos valores contidos no array;
 // A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.
+console.log((numbers.reduce((sum,current) => sum + current, 0)) / numbers.length);
 
 
 // Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”;
