affb727ded46d9468e735dfa3ffcbcf45a265e04
KennyKoyama <kennysk1982@gmail.com>
2022-09-22 19:07:08 -0300

    Sort items from the array and print

---
+++ trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia02JSArrayELoopFor/exerciseBonus.js
@@ -0,0 +1,6 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Ordene o array numbers em ordem crescente e imprima seus valores
+let sorted = numbers.sort((firstNumber, secondNumber) => firstNumber - secondNumber);
+console.log(sorted);
+
