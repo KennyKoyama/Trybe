bc88866cb01768c2ca19076307a27695f9644c19
KennyKoyama <kennysk1982@gmail.com>
2022-09-26 17:35:20 -0300

    Lesson 3, use for/in to print the key and value of an object

--- trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia04ObjetosEFuncoes/lessons.js
+++ trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia04ObjetosEFuncoes/lessons.js
@@ -27,3 +27,14 @@ for (const key in names) {
 };
 
 
+// Usando o objeto abaixo, utilize for/in e imprima um console.log
+// com as chaves e valores desse objeto.
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+};
+
+for (const key in car) {
+    console.log(key + ':', car[key]);
+};
