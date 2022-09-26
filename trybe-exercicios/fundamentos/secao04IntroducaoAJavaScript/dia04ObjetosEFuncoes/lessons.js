74c0061ef87c2090978a37f8ec4b7157e24f7c19
KennyKoyama <kennysk1982@gmail.com>
2022-09-26 16:22:24 -0300

    Lesson 2, use for/in to print a message with each value of the object

--- trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia04ObjetosEFuncoes/lessons.js
+++ trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia04ObjetosEFuncoes/lessons.js
@@ -1,3 +1,4 @@
+// Crie uma variável player e atribua um objeto contendo as variáveis listadas abaixo:
 let name = 'Marta';
 let lastName = 'Silva';
 let age = 34;
@@ -11,3 +12,18 @@ let player = {
 };
 
 console.log(`Player ${player.name} ${player.lastName}, ${player.age} years old, won ${player.medals['golden']} Gold Medals and ${player.medals['silver']} Silver Medals.`);
+
+
+// Usando o objeto abaixo, utilize for/in e imprima no console a mensagem 'Olá xxxxx' para cada nome,
+// substituindo o xxxxx pelo nome em questão.
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge',
+};
+
+for (const key in names) {
+        console.log(`Olá ${names[key]}`);
+};
+
+
