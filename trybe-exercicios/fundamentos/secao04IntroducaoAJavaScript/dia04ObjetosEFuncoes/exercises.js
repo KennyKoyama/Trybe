0802aeb5534e8ea4f922c9a4cfe2d556022ce61c
KennyKoyama <kennysk1982@gmail.com>
2022-09-26 21:03:09 -0300

    Add a new key and value to the object

--- trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia04ObjetosEFuncoes/exercises.js
+++ trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia04ObjetosEFuncoes/exercises.js
@@ -2,11 +2,17 @@ let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
+};
 
 
- // Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
- // Use a sintaxe meuObjeto.chave. Valor esperado no console:
- console.log('Bem vinda', info.personagem)
+// Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
+// Use a sintaxe meuObjeto.chave. Valor esperado no console:
+console.log('Bem vinda', info.personagem)
+
+
+// Insira no objeto uma nova propriedade com o nome de chave ‘recorrente’ e o valor ‘Sim’ e,
+// em seguida, imprima o objeto no console. Use a sintaxe meuObjeto['chave'] = valor.
+info['recorrente'] = 'Sim';
+console.log(info)
 
 
