0bd49d3b03bd20ef1f4bc8dada37d3dfcb35db06
KennyKoyama <kennysk1982@gmail.com>
2022-09-27 00:45:28 -0300

    Create a function to check if a string includes the substring

--- trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia04ObjetosEFuncoes/exercises.js
+++ trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia04ObjetosEFuncoes/exercises.js
@@ -156,3 +156,12 @@ function totalSum(num) {
     return total;
 };
 console.log(totalSum(5));
+
+
+// Crie uma função que receba uma string word e outra string ending. Verifique se a string ending
+// é o final da string word. Considere que a string ending sempre será menor que a string word.
+function checkSubString(word, sub) {
+    if (word.includes(sub)) return true;
+    return false;
+};
+console.log(checkSubString('trybe', 'be'));
