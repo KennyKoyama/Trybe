36f825eda7fcefce31eb7f33472e1ae2e769e21a
KennyKoyama <kennysk1982@gmail.com>
2022-09-26 21:06:15 -0300

    Print all keys of the object using for/in

--- trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia04ObjetosEFuncoes/exercises.js
+++ trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia04ObjetosEFuncoes/exercises.js
@@ -7,12 +7,18 @@ let info = {
 
 // Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
 // Use a sintaxe meuObjeto.chave. Valor esperado no console:
-console.log('Bem vinda', info.personagem)
+console.log('Bem vinda', info.personagem);
 
 
 // Insira no objeto uma nova propriedade com o nome de chave ‘recorrente’ e o valor ‘Sim’ e,
 // em seguida, imprima o objeto no console. Use a sintaxe meuObjeto['chave'] = valor.
 info['recorrente'] = 'Sim';
-console.log(info)
+console.log(info);
+
+
+// Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console:
+for (const key in info) {
+    console.log(key);
+}
 
 
