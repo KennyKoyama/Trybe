32f3d3866b26a790812098138fe6d8433eedacda
KennyKoyama <kennysk1982@gmail.com>
2022-09-27 02:42:13 -0300

    Create a function to count the amount of each fruit, convert the fruit name to plural if more than 1 and print a sentence

--- trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia04ObjetosEFuncoes/exercisesBonus.js
+++ trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia04ObjetosEFuncoes/exercisesBonus.js
@@ -42,4 +42,47 @@ function arrayOfNumbers(values) {
     console.log(evens);
     return evens;
 };
-arrayOfNumbers([[1, 2], [3,4,5,6], [7,8,9,10]])
+arrayOfNumbers([[1, 2], [3,4,5,6], [7,8,9,10]]);
+
+
+// A partir do array de frutas basket, retorne um objeto que contenha o nome da fruta como chave e a quantidade
+// de vezes que ela aparece no array como valor.
+function countFruits(basket) {
+    let countList = {};
+    for (let fruit of basket) {
+        let count = 0;
+        for (const checkFruit of basket) {
+            if (checkFruit === fruit) count += 1;
+        };
+        if (count > 1) fruit = fruit + 's';
+        countList[fruit] = count;
+    };
+    console.log(countList);
+    let stringsList = [];
+    for (const key in countList) {
+        stringsList.push(countList[key] + ' ' + key);
+    };
+    let sentence = `Sua cesta possui: ${stringsList.join(', ')}`;
+    console.log(sentence)
+}
+countFruits([
+    'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+    'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+    'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+    'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+    'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+    'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+    'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+    'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+    'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+    'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+    'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+    'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+    'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+    'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+    'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+    'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+    'Banana', 'Pera', 'Abacate', 'Uva']);
+
+
+    
