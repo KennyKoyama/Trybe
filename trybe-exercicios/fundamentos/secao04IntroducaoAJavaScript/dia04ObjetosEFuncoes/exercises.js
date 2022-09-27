6d5c051c9b84d443d9ccedf3e10efde38ade35dc
KennyKoyama <kennysk1982@gmail.com>
2022-09-26 22:11:38 -0300

    Create a new object and print them together

--- trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia04ObjetosEFuncoes/exercises.js
+++ trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia04ObjetosEFuncoes/exercises.js
@@ -28,3 +28,17 @@ for (const key in info) {
 }
 
 
+// Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores:
+// ‘Tio Patinhas’, ‘Christmas on Bear Mountain, Dell’s Four Color Comics #178’, ‘O último MacPatinhas’, ‘Sim’.
+// Então, imprima os valores de cada objeto juntos, de acordo com cada uma das chaves.
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim'
+};
+
+console.log(info.personagem + ' e ' + info2.personagem);
+console.log(info.origem + ' e ' + info2.origem);
+console.log(info.nota + ' e ' + info2.nota);
+console.log(info.recorrente === 'Sim' && info2.recorrente === 'Sim' ? 'Ambos recorrentes' : `${info.recorrente} e ${info2.recorrente}`);
