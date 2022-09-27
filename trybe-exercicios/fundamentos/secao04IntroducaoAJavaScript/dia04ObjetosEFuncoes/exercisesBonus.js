c7fb1fd1b669952eb082a05dafb1f003e3f431e7
KennyKoyama <kennysk1982@gmail.com>
2022-09-27 02:19:54 -0300

    Create a function to filter the even numbers from a vector

--- trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia04ObjetosEFuncoes/exercisesBonus.js
+++ trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia04ObjetosEFuncoes/exercisesBonus.js
@@ -28,3 +28,18 @@ function convertRoman(roman) {
 };
 console.log(convertRoman('MMCDXXI'));
 
+
+// Crie uma função chamada arrayOfNumbers que receberá a variável vector como parâmetro.
+// Através de um loop for, percorra essa variável, busque os números pares e os adicione
+// a um novo array, que deverá ser retornado ao final pela função.
+function arrayOfNumbers(values) {
+    let evens = [];
+    for (const subArray of values) {
+        for (const number of subArray) {
+            if (number % 2 === 0) evens.push(number);
+        };
+    };
+    console.log(evens);
+    return evens;
+};
+arrayOfNumbers([[1, 2], [3,4,5,6], [7,8,9,10]])
