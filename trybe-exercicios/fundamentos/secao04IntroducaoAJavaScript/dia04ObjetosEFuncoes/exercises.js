19e8fc71f3f9bae9a639bfaec1094a507b40f277
KennyKoyama <kennysk1982@gmail.com>
2022-09-26 23:31:46 -0300

    Print the sentence including name, last name and favorite book

--- trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia04ObjetosEFuncoes/exercises.js
+++ trybe-exercicios/fundamentos/secao04IntroducaoAJavaScript/dia04ObjetosEFuncoes/exercises.js
@@ -42,3 +42,31 @@ console.log(info.personagem + ' e ' + info2.personagem);
 console.log(info.origem + ' e ' + info2.origem);
 console.log(info.nota + ' e ' + info2.nota);
 console.log(info.recorrente === 'Sim' && info2.recorrente === 'Sim' ? 'Ambos recorrentes' : `${info.recorrente} e ${info2.recorrente}`);
+
+
+// Usando o objeto abaixo, faça os exercícios a seguir:
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+
+
+// Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um
+// console.log no seguinte formato: “O livro favorito de Julia Pessoa se chama ‘O Pior Dia de Todos’”.
+console.log(`O livro favorito de ${leitor.nome} ${leitor.sobrenome} se chama '${leitor.livrosFavoritos[0]['titulo']}'`)
+
+// Adicione um novo livro favorito na chave livrosFavoritos, que é um array. Atribua a essa chave um
+// objeto contendo as seguintes informações:
+
+
+// Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: “Julia tem
+// <quantidade> livros favoritos”, onde “<quantidade>” é um número gerado automaticamente pelo seu código.
+
