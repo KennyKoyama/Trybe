1302694cf9f1afd9b2a39a23ec6730600088812d
KennyKoyama <kennysk1982@gmail.com>
2022-09-30 18:58:59 -0300

    Create a function that change the color of Meu Top 3 text when mouse over

--- trybe-exercicios/fundamentos/secao05JSDomEventosEWebStorage/dia03JSEventos/lesson.js
+++ trybe-exercicios/fundamentos/secao05JSDomEventosEWebStorage/dia03JSEventos/lesson.js
@@ -38,6 +38,7 @@ myWebpage.addEventListener("dblclick", () => window.location.href = "https://www
 
 // - Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+myWebpage.addEventListener("mouseover", () => myWebpage.style.color = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`)
 
 // Segue abaixo um exemplo do uso de event.target:
 
