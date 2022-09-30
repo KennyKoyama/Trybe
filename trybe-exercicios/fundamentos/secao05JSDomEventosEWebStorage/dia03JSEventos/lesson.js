5e0ae766928ec3762fe5cefdacc84f2a1cefd23d
KennyKoyama <kennysk1982@gmail.com>
2022-09-30 18:50:47 -0300

    Create a function that after double click in My Top 3 element will redirect to my portfolio

--- trybe-exercicios/fundamentos/secao05JSDomEventosEWebStorage/dia03JSEventos/lesson.js
+++ trybe-exercicios/fundamentos/secao05JSDomEventosEWebStorage/dia03JSEventos/lesson.js
@@ -34,6 +34,7 @@ input.addEventListener("keyup", () => {
 // - Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 //  - Que tal redirecionar para seu portfólio?
+myWebpage.addEventListener("dblclick", () => window.location.href = "https://www.hackerrank.com/kennykoyama");
 
 // - Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
