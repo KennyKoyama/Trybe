1d30e3c0fe6843390ca2f3a207287111a7af34b6
KennyKoyama <kennysk1982@gmail.com>
2022-10-01 03:18:22 -0300

    Part 7: Create a function that receives a string and create a span element inside my tasks

--- trybe-exercicios/fundamentos/secao05JSDomEventosEWebStorage/dia03JSEventos/exercise.js
+++ trybe-exercicios/fundamentos/secao05JSDomEventosEWebStorage/dia03JSEventos/exercise.js
@@ -145,3 +145,17 @@ function zoomOut(element) {
 }
 
 
+// Parte 7
+// Implemente uma função que adicione uma tarefa personalizada ao calendário.
+// A função deve receber como parâmetro a string com o nome da tarefa (ex: “cozinhar”) e
+// criar dinamicamente um elemento com a tag <span> contendo a tarefa.
+// O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui
+// a classe "my-tasks".
+const myTasks = document.querySelector('.my-tasks');
+function addMyTask(task) {
+    const project = document.createElement('span');
+    project.innerHTML = task;
+    myTasks.appendChild(project);
+}
+addMyTask('Projeto: ');
+
