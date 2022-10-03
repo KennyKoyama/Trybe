8568def2b5bc282aa2154befd4df19748932dd5b
KennyKoyama <kennysk1982@gmail.com>
2022-10-03 19:02:07 -0300

    Make some tests with web storage

---
+++ trybe-exercicios/fundamentos/secao05JSDomEventosEWebStorage/dia04JSWebStorage/lessons.js
@@ -0,0 +1,5 @@
+localStorage.setItem('firstname', 'Adamsss')
+localStorage.setItem('lastname', 'Smithsss')
+localStorage.getItem('lastname')
+localStorage.setItem('age', 39)
+document.cookie = 'name: ken; expires=Thu, 18 Jan 2023 12:00:00 UTC; path=/';
