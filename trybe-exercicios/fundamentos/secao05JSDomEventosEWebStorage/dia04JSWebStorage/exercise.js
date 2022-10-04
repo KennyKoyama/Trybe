ca01801d4a51da97f550a2de1469e5db257df746
KennyKoyama <kennysk1982@gmail.com>
2022-10-03 21:39:11 -0300

    Declare constants for all ids

---
+++ trybe-exercicios/fundamentos/secao05JSDomEventosEWebStorage/dia04JSWebStorage/exercise.js
@@ -0,0 +1,12 @@
+const main = document.querySelector('main');
+const mainStyle = main.style;
+const bgColorBtn = document.querySelector('#bgColorBtn');
+const fontColorBtn = document.querySelector('#fontColorBtn');
+const fontSizeBtn = document.querySelector('#fontSizeBtn');
+const textLineGapBtn = document.querySelector('#textLineGapBtn');
+const fontFamilyArialBtn = document.querySelector('#fontFamilyArialBtn');
+const fontFamilyVerdanaBtn = document.querySelector('#fontFamilyArialBtn');
+const fontFamilyImpactBtn = document.querySelector('#fontFamilyArialBtn');
+const fontFamilyGeorgiaBtn = document.querySelector('#fontFamilyArialBtn');
+const fontFamilyCursiveBtn = document.querySelector('#fontFamilyArialBtn');
+
