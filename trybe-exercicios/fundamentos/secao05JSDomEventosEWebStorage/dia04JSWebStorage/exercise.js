3272ff1af6b74d020b8c5276115c822c68076938
KennyKoyama <kennysk1982@gmail.com>
2022-10-04 01:46:24 -0300

    Add localStorage set and get

--- trybe-exercicios/fundamentos/secao05JSDomEventosEWebStorage/dia04JSWebStorage/exercise.js
+++ trybe-exercicios/fundamentos/secao05JSDomEventosEWebStorage/dia04JSWebStorage/exercise.js
@@ -1,33 +1,56 @@
 const main = document.querySelector('main');
 const mainStyle = main.style;
 const bgColorPicker = document.querySelector('#bgColorPicker');
-const bgColorBtn = document.querySelector('#bgColorBtn');
 const fontColorPicker = document.querySelector('#fontColorPicker');
-const fontColorBtn = document.querySelector('#fontColorBtn');
 const fontSizeSlider = document.querySelector('#fontSizeSlider');
-const fontSizeBtn = document.querySelector('#fontSizeBtn');
 const textLineGapSlider = document.querySelector('#textLineGapSlider');
-const textLineGapBtn = document.querySelector('#textLineGapBtn');
 const fontFamilyArialBtn = document.querySelector('#fontFamilyArialBtn');
 const fontFamilyVerdanaBtn = document.querySelector('#fontFamilyVerdanaBtn');
 const fontFamilyImpactBtn = document.querySelector('#fontFamilyImpactBtn');
 const fontFamilyGeorgiaBtn = document.querySelector('#fontFamilyGeorgiaBtn');
 const fontFamilyCursiveBtn = document.querySelector('#fontFamilyCursiveBtn');
 
-// bgColorBtn.addEventListener('click', () => mainStyle.backgroundColor = bgColorPicker.value);
-bgColorPicker.addEventListener('input', () => mainStyle.backgroundColor = bgColorPicker.value);
+mainStyle.backgroundColor = localStorage.getItem('bgColor');
+mainStyle.color = localStorage.getItem('fontColor');
+mainStyle.fontSize = localStorage.getItem('fontSize');
+mainStyle.lineHeight = localStorage.getItem('lineGap');
+mainStyle.fontFamily = localStorage.getItem('fontFamily');
 
-// fontColorBtn.addEventListener('click', () => mainStyle.color = fontColorPicker.value);
-fontColorPicker.addEventListener('input', () => mainStyle.color = fontColorPicker.value);
 
-// fontSizeBtn.addEventListener('click', () => mainStyle.fontSize = fontSizeSlider.value + 'px');
-fontSizeSlider.addEventListener('input', () => mainStyle.fontSize = fontSizeSlider.value + 'px');
+bgColorPicker.addEventListener('input', () => {
+    mainStyle.backgroundColor = bgColorPicker.value;
+    localStorage.setItem('bgColor', bgColorPicker.value);
+});
+fontColorPicker.addEventListener('input', () => {
+    mainStyle.color = fontColorPicker.value;
+    localStorage.setItem('fontColor', fontColorPicker.value);
+});
+fontSizeSlider.addEventListener('input', () => {
+    mainStyle.fontSize = fontSizeSlider.value + 'px';
+    localStorage.setItem('fontSize', fontSizeSlider.value + 'px');
+});
+textLineGapSlider.addEventListener('input', () => {
+    mainStyle.lineHeight = textLineGapSlider.value;
+    localStorage.setItem('lineGap', textLineGapSlider.value);
+});
+fontFamilyArialBtn.addEventListener('click', () => {
+    mainStyle.fontFamily = 'Arial, sans-serif';
+    localStorage.setItem('fontFamily', 'Arial, sans-serif');
+});
+fontFamilyVerdanaBtn.addEventListener('click', () => {
+    mainStyle.fontFamily = 'Verdana, sans-serif';
+    localStorage.setItem('fontFamily', 'Verdana, sans-serif');
+});
+fontFamilyImpactBtn.addEventListener('click', () => {
+    mainStyle.fontFamily = 'Impact, sans-serif';
+    localStorage.setItem('fontFamily', 'Impact, sans-serif');
+});
+fontFamilyGeorgiaBtn.addEventListener('click', () => {
+    mainStyle.fontFamily = 'Georgia, sans-serif';
+    localStorage.setItem('fontFamily', 'Georgia, sans-serif');
+});
+fontFamilyCursiveBtn.addEventListener('click', () => {
+    mainStyle.fontFamily = 'cursive, sans-serif';
+    localStorage.setItem('fontFamily', 'cursive, sans-serif');
+});
 
-// textLineGapBtn.addEventListener('click', () => mainStyle.lineHeight = textLineGapSlider.value);
-textLineGapSlider.addEventListener('input', () => mainStyle.lineHeight = textLineGapSlider.value);
-
-fontFamilyArialBtn.addEventListener('click', () => mainStyle.fontFamily = 'Arial, sans-serif');
-fontFamilyVerdanaBtn.addEventListener('click', () => mainStyle.fontFamily = 'Verdana, sans-serif');
-fontFamilyImpactBtn.addEventListener('click', () => mainStyle.fontFamily = 'Impact, sans-serif');
-fontFamilyGeorgiaBtn.addEventListener('click', () => mainStyle.fontFamily = 'Georgia, sans-serif');
-fontFamilyCursiveBtn.addEventListener('click', () => mainStyle.fontFamily = 'cursive, sans-serif');
