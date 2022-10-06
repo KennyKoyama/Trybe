8e8e320141b35cc964a5e5b7b2ebe8d8f23beb1b
KennyKoyama <kennysk1982@gmail.com>
2022-10-06 19:28:40 -0300

    Create a event listener to the text input to with the method preventDefault to let only accept the letter 'a'

--- trybe-exercicios/fundamentos/secao06HTMLECSSFormsFlexboxEResponsivo/dia01HTMLECSSForms/lessons/script.js
+++ trybe-exercicios/fundamentos/secao06HTMLECSSFormsFlexboxEResponsivo/dia01HTMLECSSForms/lessons/script.js
@@ -10,3 +10,4 @@ HREF_LINK.addEventListener('click', (event) => event.preventDefault());
 INPUT_CHECKBOX.addEventListener('click', (event) => event.preventDefault());
 
 // Adicione um evento keypress no elemento input type="text" que possui o id input-text. Na função do evento, implemente o método .preventDefault() de forma que somente o caractere a (letra ‘a’ minúscula) possa ser digitado na caixinha. (dica: para capturar a tecla digitada, utilize o event.key)
+INPUT_TEXT.addEventListener('keypress', (event) => event.key !== 'a' ? event.preventDefault() : 'a');
