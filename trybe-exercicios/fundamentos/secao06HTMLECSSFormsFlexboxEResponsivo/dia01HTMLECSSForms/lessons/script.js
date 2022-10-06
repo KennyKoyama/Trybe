0f84f76e19c3daabf95e21287d89a680e8ade857
KennyKoyama <kennysk1982@gmail.com>
2022-10-06 19:22:26 -0300

    Create a event listener to the checkbox with the method preventDefault

--- trybe-exercicios/fundamentos/secao06HTMLECSSFormsFlexboxEResponsivo/dia01HTMLECSSForms/lessons/script.js
+++ trybe-exercicios/fundamentos/secao06HTMLECSSFormsFlexboxEResponsivo/dia01HTMLECSSForms/lessons/script.js
@@ -7,5 +7,6 @@ const HREF_LINK = document.querySelector("#href");
 HREF_LINK.addEventListener('click', (event) => event.preventDefault());
 
 // Adicione um evento click no elemento checkbox que possui o id input-checkbox. Na função do evento, implemente o método .preventDefault() para prevenir o comportamento padrão do elemento. Ou seja, ao clicar na caixinha, nada deve acontecer.
+INPUT_CHECKBOX.addEventListener('click', (event) => event.preventDefault());
 
 // Adicione um evento keypress no elemento input type="text" que possui o id input-text. Na função do evento, implemente o método .preventDefault() de forma que somente o caractere a (letra ‘a’ minúscula) possa ser digitado na caixinha. (dica: para capturar a tecla digitada, utilize o event.key)
