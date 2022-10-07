02decb4b72795f6396903d8117bcaa9bf816ed35
KennyKoyama <kennysk1982@gmail.com>
2022-10-06 21:45:41 -0300

    Create a event listener to the submit button with the method preventDefault

---
+++ trybe-exercicios/fundamentos/secao06HTMLECSSFormsFlexboxEResponsivo/dia01HTMLECSSForms/exercise/script.js
@@ -0,0 +1,9 @@
+// Interrompa o comportamento padrão do botão submit utilizando o método preventDefault().
+const submitButton = document.querySelector('#submit-button');
+submitButton.addEventListener('click', (event) => event.preventDefault());
+
+// Crie um botão que limpe as informações contidas nos campos.
+
+// (Bônus) A TrybeTrip precisa muito de fotos para divulgar seus concursos. Tendo isso em mente, faça com que somente quem autorizar o uso de imagens possa enviar suas informações.
+
+// (Bônus) Faça a validação dos campos com limite de caracteres. Caso não estejam dentro do esperado ao clicar no botão de submit, um alerta deve ser mostrado com a mensagem: ‘Dados Inválidos’. Caso contrário, a mensagem ‘Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.’ deverá aparecer na tela.
