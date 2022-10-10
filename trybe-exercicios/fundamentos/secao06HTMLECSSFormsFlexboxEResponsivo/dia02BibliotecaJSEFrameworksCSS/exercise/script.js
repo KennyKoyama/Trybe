037363643e691708d439a3dc858b4971db5b5291
KennyKoyama <kennysk1982@gmail.com>
2022-10-10 04:19:57 -0300

    Make the validation style looks better

--- trybe-exercicios/fundamentos/secao06HTMLECSSFormsFlexboxEResponsivo/dia02BibliotecaJSEFrameworksCSS/exercise/script.js
+++ trybe-exercicios/fundamentos/secao06HTMLECSSFormsFlexboxEResponsivo/dia02BibliotecaJSEFrameworksCSS/exercise/script.js
@@ -1,14 +1,12 @@
-// Interrompa o comportamento padrão do botão submit utilizando o método preventDefault().
 const submitButton = document.querySelector('#submit-btn');
-// submitButton.addEventListener('click', (event) => event.preventDefault());
 const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
 const textInput = document.querySelector('#answer');
 const dateInput = document.querySelector('#datepicker');
-const destinyInput = document.querySelectorAll('input[name="preferred-destiny"]');
-
-// (Bônus) A TrybeTrip precisa muito de fotos para divulgar seus concursos. Tendo isso em mente, faça com que somente quem autorizar o uso de imagens possa enviar suas informações.
+const destinyInput = document.querySelectorAll('.radio');
 const imageRights = document.querySelector('#image-rights-checkbox');
+// Interrompa o comportamento padrão do botão submit utilizando o método preventDefault().
+// (Bônus) A TrybeTrip precisa muito de fotos para divulgar seus concursos. Tendo isso em mente, faça com que somente quem autorizar o uso de imagens possa enviar suas informações.
 submitButton.addEventListener('click', (event) => {
   if (!isValid()) {
     event.preventDefault();
@@ -30,35 +28,44 @@ const checkEmail = () => emailInput.value.length > 10;
 const checkText = () => textInput.value.length > 50;
 const checkDate = () => dateInput.value.length > 0;
 const checkImgRights = () => imageRights.checked;
-const checkDestiny = document.querySelectorAll('input[name="preferred-destiny"]');
+const checkRadio = () => [...destinyInput].map((current) => current.checked).includes(true);
 const classValidate = (bool) => bool ? 'is-valid' : 'is-invalid';
 const typeText = 'form__input form-control';
 const typeCheckbox = 'form__input form-check-input';
 const typeDate = 'left-margin date-input';
 const typeRadio = 'form-check-input';
+
 function toggleClass(selector, check) {
   const findType = selector.type;
   const findID = selector.id;
   if (findType === 'text' || findType === 'textarea') selector.className = typeText;
-  if (findType === 'checkbox') selector.className = typeCheckbox;
-  // if (findType === 'radio') selector.className = typeRadio;
   if (findID === 'datepicker') selector.className = typeDate;
+  if (findType === 'checkbox') {
+    selector.className = typeCheckbox;
+    selector.labels[0].className = 'form-check-label';
+    if (!selector.checked) selector.labels[0].classList.add('is-invalid-radio');
+  };
   selector.classList.add(classValidate(check));
+  destinyInput.forEach(element => {
+    const label = element.labels[0];
+    element.className = 'form-check-input radio';
+    label.className = 'form-check-label';
+    if (!checkRadio()) {
+      console.log(element)
+      element.classList.add('is-invalid-radio');
+      label.classList.add('is-invalid-radio');
+    };
+  });
 }
 
 function isValid() {
-  const checkDestiny = document.querySelectorAll('input[name="preferred-destiny"]');
-  let checkRadio = false;
-  for (const option of checkDestiny) {
-    if (option.checked) radio = true;
-  };
   toggleClass(nameInput, checkName())
   toggleClass(emailInput, checkEmail())
   toggleClass(textInput, checkText())
   toggleClass(dateInput, checkDate())
   toggleClass(imageRights, checkImgRights())
 
-  const result = checkName() && checkEmail() && checkText() && checkDate() && checkRadio;
+  const result = checkName() && checkEmail() && checkText() && checkDate() && checkRadio();
   return result
 };
 
