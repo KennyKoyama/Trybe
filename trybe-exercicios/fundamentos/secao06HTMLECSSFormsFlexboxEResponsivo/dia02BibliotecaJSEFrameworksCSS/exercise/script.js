b57a0d2f165bb070b68eb7e1428bbdc5f4e6fca2
KennyKoyama <kennysk1982@gmail.com>
2022-10-10 01:47:20 -0300

    Make a custom validation style

--- trybe-exercicios/fundamentos/secao06HTMLECSSFormsFlexboxEResponsivo/dia02BibliotecaJSEFrameworksCSS/exercise/script.js
+++ trybe-exercicios/fundamentos/secao06HTMLECSSFormsFlexboxEResponsivo/dia02BibliotecaJSEFrameworksCSS/exercise/script.js
@@ -1,64 +1,96 @@
-import JustValidate from 'just-validate';
-import bootstrap from 'bootstrap';
-
 // Interrompa o comportamento padrão do botão submit utilizando o método preventDefault().
-const submitButton = document.querySelector('#submit-button');
-submitButton.addEventListener('click', (event) => event.preventDefault());
-
-// Crie um botão que limpe as informações contidas nos campos.
-const buttonsFieldset = document.querySelector('#buttons-fieldset');
+const submitButton = document.querySelector('#submit-btn');
+// submitButton.addEventListener('click', (event) => event.preventDefault());
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const textInput = document.querySelector('#answer');
+const dateInput = document.querySelector('#datepicker');
+const destinyInput = document.querySelectorAll('input[name="preferred-destiny"]');
 
 // (Bônus) A TrybeTrip precisa muito de fotos para divulgar seus concursos. Tendo isso em mente, faça com que somente quem autorizar o uso de imagens possa enviar suas informações.
-
-// (Bônus) Faça a validação dos campos com limite de caracteres. Caso não estejam dentro do esperado ao clicar no botão de submit, um alerta deve ser mostrado com a mensagem: ‘Dados Inválidos’. Caso contrário, a mensagem ‘Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.’ deverá aparecer na tela.
-
-//Use Just Validate library
-const validation = new JustValidate('#form', {
-  errorFieldCssClass: 'is-invalid',
+const imageRights = document.querySelector('#image-rights-checkbox');
+submitButton.addEventListener('click', (event) => {
+  if (!isValid()) {
+    event.preventDefault();
+    return;
+    // return alert('Dados Inválidos');
+  };
+  if (!imageRights.checked) {
+    alert('Você precisa concordar com o direito de uso de imagem para conseguir enviar.');
+    event.preventDefault();
+  } else {
+    const fullName = document.querySelector('#name-input').value;
+    alert(`Formulário enviado com sucesso! Boa sorte ${fullName}! Obrigado por participar do concurso TrybeTrip.`);
+  }
 });
 
-validation
-  .addField('#name', [
-    {
-      rule: 'minLength',
-      value: 5,
-    },
-    {
-      rule: 'maxLength',
-      value: 50,
-    },
-  ])
-  .addField('#email', [
-    {
-      rule: 'required',
-      errorMessage: 'Email is required',
-    },
-    {
-      rule: 'email',
-      errorMessage: 'Email is invalid!',
-    },
-  ])
-  .addField('#image-rights-checkbox', [
-    {
-      rule: 'required',
-      errorMessage: 'Image rights is required',
-    }
-  ]);
+// (Bônus) Faça a validação dos campos com limite de caracteres. Caso não estejam dentro do esperado ao clicar no botão de submit, um alerta deve ser mostrado com a mensagem: ‘Dados Inválidos’. Caso contrário, a mensagem ‘Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.’ deverá aparecer na tela.
+const checkName = () => nameInput.value.length > 10;
+const checkEmail = () => emailInput.value.length > 10;
+const checkText = () => textInput.value.length > 50;
+const checkDate = () => dateInput.value.length > 0;
+const checkImgRights = () => imageRights.checked;
+const checkDestiny = document.querySelectorAll('input[name="preferred-destiny"]');
+const classValidate = (bool) => bool ? 'is-valid' : 'is-invalid';
+const typeText = 'form__input form-control';
+const typeCheckbox = 'form__input form-check-input';
+const typeDate = 'left-margin date-input';
+const typeRadio = 'form-check-input';
+function toggleClass(selector, check) {
+  const findType = selector.type;
+  const findID = selector.id;
+  if (findType === 'text' || findType === 'textarea') selector.className = typeText;
+  if (findType === 'checkbox') selector.className = typeCheckbox;
+  // if (findType === 'radio') selector.className = typeRadio;
+  if (findID === 'datepicker') selector.className = typeDate;
+  selector.classList.add(classValidate(check));
+}
 
-// Bootstrap validation
-(() => {
-  'use strict'
-const forms = document.querySelectorAll('.needs-validation')
+function isValid() {
+  const checkDestiny = document.querySelectorAll('input[name="preferred-destiny"]');
+  let checkRadio = false;
+  for (const option of checkDestiny) {
+    if (option.checked) radio = true;
+  };
+  toggleClass(nameInput, checkName())
+  toggleClass(emailInput, checkEmail())
+  toggleClass(textInput, checkText())
+  toggleClass(dateInput, checkDate())
+  toggleClass(imageRights, checkImgRights())
 
-// Loop over them and prevent submission
-Array.from(forms).forEach(form => {
-  form.addEventListener('submit', event => {
-    if (!form.checkValidity()) {
-      event.preventDefault()
-      event.stopPropagation()
-    }
+  const result = checkName() && checkEmail() && checkText() && checkDate() && checkRadio;
+  return result
+};
 
-    form.classList.add('was-validated')
-  }, false)
-})
-})()
+//Use Just Validate library
+// const validation = new JustValidate('#form', {
+//   errorFieldCssClass: 'is-invalid',
+// });
+
+// validation
+//   .addField('#name', [
+//     {
+//       rule: 'minLength',
+//       value: 5,
+//     },
+//     {
+//       rule: 'maxLength',
+//       value: 50,
+//     },
+//   ])
+//   .addField('#email', [
+//     {
+//       rule: 'required',
+//       errorMessage: 'Email is required',
+//     },
+//     {
+//       rule: 'email',
+//       errorMessage: 'Email is invalid!',
+//     },
+//   ])
+//   .addField('#image-rights-checkbox', [
+//     {
+//       rule: 'required',
+//       errorMessage: 'Image rights is required',
+//     }
+//   ]);
