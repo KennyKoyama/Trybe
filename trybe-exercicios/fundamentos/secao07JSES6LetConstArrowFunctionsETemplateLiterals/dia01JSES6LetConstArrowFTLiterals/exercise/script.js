367ff6dc9e1904fb1b765352582c43ec0dc5466b
KennyKoyama <kennysk1982@gmail.com>
2022-10-18 00:49:08 -0300

    Solve the first exercise

--- trybe-exercicios/fundamentos/secao07JSES6LetConstArrowFunctionsETemplateLiterals/dia01JSES6LetConstArrowFTLiterals/exercise/script.js
+++ trybe-exercicios/fundamentos/secao07JSES6LetConstArrowFunctionsETemplateLiterals/dia01JSES6LetConstArrowFTLiterals/exercise/script.js
@@ -1,24 +1,35 @@
 // Parte I
 // 🚀 Exercício 1
+// Altere o tipo das variáveis(var) para let ou const para que respeitem o escopo em 
+// que foram declaradas, e altere a função utilizando arrow function e template literals.
+// Modifique as variáveis para que respeitem o escopo onde estão declaradas;
 function testingScope(escopo) {
   if (escopo === true) {
-    var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
     ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
     console.log(ifScope);
   } else {
-    var elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
+    const elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
     console.log(elseScope);
   }
-  console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+  // console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
 }
-
 testingScope(true);
+testingScope(false);
+
+// Modifique a estrutura da função para que ela seja uma arrow function;
+// Modifique as concatenações para template literals.
+const ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+const testingScopeArrow = escopo => escopo ? console.log(`${ifScope} ótimo, fui utilizada no escopo !`)
+                                  : console.log('Não devo ser utilizada fora do meu escopo (else)');
+testingScopeArrow(true);
+testingScopeArrow(false);
+
+
 // Crie uma função que retorne um array em ordem crescente.
-
-
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-const sortOddsAndEvens = () => {
+const sortOddsAndEvens = (numberList) => `Os números ${numberList.sort((a, b) => a - b)} se encontram ordenados de forma crescente!`;
 // Seu código aqui.
 // Crie uma função que faça com que o array oddsAndEvens fique em ordem crescente;
 // Utilize template literals para que a chamada 
@@ -26,5 +37,5 @@ const sortOddsAndEvens = () => {
 // retorne a frase “Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!”.
 // Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort().
 // Spoiler: É possível realizar uma função que ordene qualquer array de números.
-}
-console.log(oddsAndEvens); // será necessário alterar essa linha 😉
+
+console.log(sortOddsAndEvens(oddsAndEvens)); // será necessário alterar essa linha 😉
