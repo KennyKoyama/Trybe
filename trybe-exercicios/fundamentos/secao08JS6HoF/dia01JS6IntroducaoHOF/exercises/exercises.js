4624d30ab543a51479ca4a2bcd7ab9bfa339ba24
KennyKoyama <kennysk1982@gmail.com>
2022-10-24 19:02:49 -0300

    Solve the third exercise

--- trybe-exercicios/fundamentos/secao08JS6HoF/dia01JS6IntroducaoHOF/exercises/exercises.js
+++ trybe-exercicios/fundamentos/secao08JS6HoF/dia01JS6IntroducaoHOF/exercises/exercises.js
@@ -29,3 +29,24 @@ const sortNumber = () => Math.ceil(Math.random() * 5);
 const isWinner = (bet, result) => bet === result ? 'Parabéns você ganhou' : 'Tente novamente';
 
 console.log(isWinner('2', sortNumber()))
+
+
+// Corretor automático de exame
+// Crie uma HOF que receberá três parâmetros:
+// O primeiro será um array de respostas corretas (soluções);
+// O segundo será um array contendo as respostas fornecidas por uma pessoa estudante;
+// O terceiro é uma função que compara os dois arrays e então dá uma pontuação baseada nos
+// acertos. Para isso, essa função usará os seguintes critérios:
+// Uma resposta correta adiciona 1 ponto à pontuação final;
+// A ausência de uma resposta não altera a pontuação (quando for “N.A”);
+// Uma resposta incorreta reduz a pontuação final em 0.5 ponto.
+// Ao final, a HOF deve retornar o total de pontos obtidos através das respostas
+// fornecidas pela pessoa estudante. Utilize os seguintes arrays:
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const autoFix = (rightAnswers, studentAnswers, funct) => funct(rightAnswers,studentAnswers).reduce((sum, curr) => sum + curr, 0);
+const score = (right, student) => student.map((studentAnswers, index) => studentAnswers === right[index] ? 1 : studentAnswers === 'N.A' ? 0 : -0.5);
+
+console.log(score(RIGHT_ANSWERS, STUDENT_ANSWERS))
+console.log(autoFix(RIGHT_ANSWERS, STUDENT_ANSWERS, score))
