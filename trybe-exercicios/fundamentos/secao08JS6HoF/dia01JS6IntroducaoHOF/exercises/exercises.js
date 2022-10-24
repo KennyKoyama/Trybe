df4018c5b89bfbfd73ed9caccde663d655c784f3
KennyKoyama <kennysk1982@gmail.com>
2022-10-24 18:40:09 -0300

    Solve the second exercise

--- trybe-exercicios/fundamentos/secao08JS6HoF/dia01JS6IntroducaoHOF/exercises/exercises.js
+++ trybe-exercicios/fundamentos/secao08JS6HoF/dia01JS6IntroducaoHOF/exercises/exercises.js
@@ -1,3 +1,4 @@
+// Nova pessoa contratada
 // Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando
 // uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para
 // criar cada pessoa contratada em seu respectivo id. A sua função deve receber como
@@ -19,3 +20,12 @@ const newEmp = (fullName) => ( { nomeCompleto: fullName, email: `${fullName.spli
 console.log(newEmployees(newEmp('Pedro Guerra'),newEmp('Luiza Drumond'),newEmp('Carla Paiva')));
 
 
+// Sorteador de loteria
+// Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número
+// aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa
+// se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma
+// string (Ex: “Tente novamente” ou “Parabéns você ganhou”).
+const sortNumber = () => Math.ceil(Math.random() * 5);
+const isWinner = (bet, result) => bet === result ? 'Parabéns você ganhou' : 'Tente novamente';
+
+console.log(isWinner('2', sortNumber()))
