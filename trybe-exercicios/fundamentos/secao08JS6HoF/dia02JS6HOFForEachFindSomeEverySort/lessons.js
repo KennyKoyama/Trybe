27c7bb2decc86dacd5387891ee310abf500b36e3
KennyKoyama <kennysk1982@gmail.com>
2022-10-25 18:45:03 -0300

    Solve the second lesson

--- trybe-exercicios/fundamentos/secao08JS6HoF/dia02JS6HOFForEachFindSomeEverySort/lessons.js
+++ trybe-exercicios/fundamentos/secao08JS6HoF/dia02JS6HOFForEachFindSomeEverySort/lessons.js
@@ -1,13 +1,31 @@
+// For Each
 const emailListInData = [
   'roberta@email.com',
   'paulo@email.com',
   'anaroberta@email.com',
   'fabiano@email.com',
 ];
-
 const showEmailList = (email) => {
   console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
 };
-
-// Adicione seu código aqui
 emailListInData.forEach((email) => showEmailList(email));
+
+
+// Find
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+const findDivisibleBy3And5 = () =>  numbers.find((number) => number % 3 === 0 && number % 5 === 0);
+console.log(findDivisibleBy3And5());
+
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+const findNameWithFiveLetters = () => names.find((name) => name.length === 5);
+console.log(findNameWithFiveLetters());
+
+const musicas = [
+  { id: '31031685', title: 'Partita in C moll BWV 997' },
+  { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+];
+function findMusic(id) {
+  return musicas.find((musica) => musica.id === id);
+};
+console.log(findMusic('31031685'));
