bdf423813791a02876f7f540c7ee64fd8d6006e1
KennyKoyama <kennysk1982@gmail.com>
2022-10-25 19:02:08 -0300

    Solve the fourth lesson

--- trybe-exercicios/fundamentos/secao08JS6HoF/dia02JS6HOFForEachFindSomeEverySort/lessons.js
+++ trybe-exercicios/fundamentos/secao08JS6HoF/dia02JS6HOFForEachFindSomeEverySort/lessons.js
@@ -46,3 +46,19 @@ const people = [
 ];
 const verifyAges = (arr, minimumAge) => arr.every((person) => person.age >= minimumAge);
 console.log(verifyAges(people, 18));
+
+
+// Sort ordem crescente
+const people2 = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+people2.sort((personA, personB) => personA.age - personB.age);
+console.log(people2);
+
+// Sort ordem decrescente
+people2.sort((personA, personB) => personB.age - personA.age);
+console.log(people2);
