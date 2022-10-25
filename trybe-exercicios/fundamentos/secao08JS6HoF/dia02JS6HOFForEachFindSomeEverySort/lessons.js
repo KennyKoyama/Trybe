e3ce881c3acd0e4467a3110fd1f1b432e5970310
KennyKoyama <kennysk1982@gmail.com>
2022-10-25 18:53:31 -0300

    Solve the third lesson

--- trybe-exercicios/fundamentos/secao08JS6HoF/dia02JS6HOFForEachFindSomeEverySort/lessons.js
+++ trybe-exercicios/fundamentos/secao08JS6HoF/dia02JS6HOFForEachFindSomeEverySort/lessons.js
@@ -29,3 +29,20 @@ function findMusic(id) {
   return musicas.find((musica) => musica.id === id);
 };
 console.log(findMusic('31031685'));
+
+
+// Some
+const names2 = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+const hasName = (arr, name) => arr.some((elem) => elem === name);
+console.log(hasName(names2, 'Ana'));
+
+// Every
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+const verifyAges = (arr, minimumAge) => arr.every((person) => person.age >= minimumAge);
+console.log(verifyAges(people, 18));
