68a37ac44f4b4b384cd66a79e247bdeed1016e3b
KennyKoyama <kennysk1982@gmail.com>
2022-10-28 18:18:51 -0300

    Solve the fourth lesson

--- trybe-exercicios/fundamentos/secao08JS6HoF/dia05JS6spreadOperatorParâmetroRestDestructuringEMais/lessons.js
+++ trybe-exercicios/fundamentos/secao08JS6HoF/dia05JS6spreadOperatorParâmetroRestDestructuringEMais/lessons.js
@@ -66,3 +66,22 @@ let numerosPares = [1, 3, 5, 6, 8, 10, 12];
 [,,,...numerosPares] = numerosPares;
 console.log(numerosPares); // [6, 8, 10, 12];
 
+
+// Do jeito que está, quando person é passado para a função getNationality o retorno
+// é João is undefined. Ajuste a função getNationality para que a chamada getNationality
+// (person) retorne João is Brazilian.
+const getNationality = ({ firstName, nationality = 'Brazilian' }) => `${firstName} is ${nationality}`;
+
+const person = {
+  firstName: 'João',
+  lastName: 'Jr II',
+};
+
+const otherPerson = {
+  firstName: 'Ivan',
+  lastName: 'Ivanovich',
+  nationality: 'Russian',
+};
+
+console.log(getNationality(otherPerson)); // Ivan is Russian
+console.log(getNationality(person));
