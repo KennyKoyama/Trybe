e609062d35f1bea19242b377da827ab5e4ff23d3
KennyKoyama <kennysk1982@gmail.com>
2022-10-28 16:25:41 -0300

    Solve the second lesson

--- trybe-exercicios/fundamentos/secao08JS6HoF/dia05JS6spreadOperatorParâmetroRestDestructuringEMais/lessons.js
+++ trybe-exercicios/fundamentos/secao08JS6HoF/dia05JS6spreadOperatorParâmetroRestDestructuringEMais/lessons.js
@@ -1,3 +1,4 @@
+// Lesson 1
 // Faça uma lista com as suas frutas favoritas
 const specialFruit = ['morango', 'banana', 'abacaxi'];
 
@@ -10,3 +11,24 @@ const fruitSalad = (fruit, additional) => {
 };
 
 console.log(fruitSalad(specialFruit, additionalItens));
+
+
+// Lesson 2
+// Imprima no console uma frase utilizando os dados do objeto criado anteriormente.
+// Para isso, utilize a desestruturação de objetos em conjunto com template literals.
+const user = {
+  name: 'Maria',
+  age: 21,
+  nationality: 'Brazilian',
+};
+
+const jobInfos = {
+  profession: 'Software engineer',
+  squad: 'Rocket Landing Logic',
+  squadInitials: 'RLL',
+};
+
+const {name , age, nationality} = user;
+const {profession, squad, squadInitials} = jobInfos;
+
+console.log(`Hi, my name is ${name}, I'm ${age} years old and I'm ${nationality}. I work as a ${profession} and my squad is ${squadInitials}-${squad}`)
