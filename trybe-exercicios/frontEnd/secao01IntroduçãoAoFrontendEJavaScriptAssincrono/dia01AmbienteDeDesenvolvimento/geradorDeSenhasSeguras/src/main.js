31609b3ad9228402f29f0e969b15eb2257b72fa8
KennyKoyama <kennysk1982@gmail.com>
2022-11-08 20:57:32 -0300

    Install clipboard-copy library

--- trybe-exercicios/frontEnd/secao01IntroduçãoAoFrontendEJavaScriptAssincrono/dia01AmbienteDeDesenvolvimento/geradorDeSenhasSeguras/src/main.js
+++ trybe-exercicios/frontEnd/secao01IntroduçãoAoFrontendEJavaScriptAssincrono/dia01AmbienteDeDesenvolvimento/geradorDeSenhasSeguras/src/main.js
@@ -1,4 +1,5 @@
 import { nanoid } from 'nanoid';
+import copy from 'clipboard-copy';
 import './styles.css';
 
 const passwordBtnEl = document.querySelector('button');
@@ -7,4 +8,11 @@ const displayPasswordEl = document.querySelector('h2');
 passwordBtnEl.addEventListener('click', () => {
   const randomPassword = nanoid();
   displayPasswordEl.innerHTML = randomPassword;
+});
+
+displayPasswordEl.addEventListener('click', () => {
+  console.log('Copy')
+  console.log(displayPasswordEl.innerHTML)
+  const currentPass = displayPasswordEl.innerHTML;
+  copy(currentPass)
 });
