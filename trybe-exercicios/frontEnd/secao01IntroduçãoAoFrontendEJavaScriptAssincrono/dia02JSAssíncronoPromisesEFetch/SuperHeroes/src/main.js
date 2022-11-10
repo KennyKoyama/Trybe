462d5282da293485fb1c98ccf2b4d0b67be2e18d
KennyKoyama <kennysk1982@gmail.com>
2022-11-09 22:47:05 -0300

    Complete the steps for the SuperHeroes app

--- trybe-exercicios/frontEnd/secao01IntroduçãoAoFrontendEJavaScriptAssincrono/dia02JSAssíncronoPromisesEFetch/SuperHeroes/src/main.js
+++ trybe-exercicios/frontEnd/secao01IntroduçãoAoFrontendEJavaScriptAssincrono/dia02JSAssíncronoPromisesEFetch/SuperHeroes/src/main.js
@@ -1,1 +1,19 @@
 import './styles.css';
+import Swal from 'sweetalert2';
+
+const HEROES_API = 'https://superheroapi.com/api.php/10229759198387812/';
+const randomID = () => Math.ceil(Math.random() * 731);
+const container = document.querySelector('#container');
+const heroImage = document.querySelector('#hero-image');
+const heroName = document.querySelector('#hero-name');
+const shuffleBtn = document.querySelector('button');
+
+shuffleBtn.addEventListener('click', () => {
+  fetch(`${HEROES_API}${randomID()}`)
+    .then(response => response.json())
+    .then(data => {
+      heroImage.src = data.image.url;
+      heroName.innerHTML = data.name;
+    })
+    .catch(error => Swal.fire(error))
+})
