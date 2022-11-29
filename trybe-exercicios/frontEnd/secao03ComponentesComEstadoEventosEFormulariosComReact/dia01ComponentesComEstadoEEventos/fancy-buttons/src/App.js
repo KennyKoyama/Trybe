2371160a0c0a7f006c1dba6174fc5f1f4980bffc
KennyKoyama <kennysk1982@gmail.com>
2022-11-29 15:14:06 -0300

    Start exercise 1

---
+++ trybe-exercicios/frontEnd/secao03ComponentesComEstadoEventosEFormulariosComReact/dia01ComponentesComEstadoEEventos/fancy-buttons/src/App.js
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+class App extends Component {
+  static propTypes = {}
+
+  render() {
+    return (
+      <div>App</div>
+    )
+  }
+}
+
+export default App
