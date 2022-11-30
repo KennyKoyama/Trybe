bc8dbe5dcb6f1bbfbece1a7e895479e69f8d0af9
KennyKoyama <kennysk1982@gmail.com>
2022-11-29 21:37:43 -0300

    Complete exercise 3

--- trybe-exercicios/frontEnd/secao03ComponentesComEstadoEventosEFormulariosComReact/dia01ComponentesComEstadoEEventos/fancy-buttons/src/App.js
+++ trybe-exercicios/frontEnd/secao03ComponentesComEstadoEventosEFormulariosComReact/dia01ComponentesComEstadoEEventos/fancy-buttons/src/App.js
@@ -3,15 +3,30 @@ import Button from './components/Button';
 
 
 class App extends Component {
+
+  state = {
+    count: 0,
+    color: 'inherit'
+  }
+  
   hello = () => console.log('Hello');
   world = () => console.log('World');
   exclamations = () => console.log('!!!');
+  counter = () => this.setState((previous) => (
+    {
+      count: previous.count + 1,
+      color: (previous.count + 1) % 2 === 0 ? 'green' : 'inherit'  
+    }
+    ));
+
   render() {
+    const {count, color} = this.state;
     return (
       <div>
         <Button func={this.hello} btnName='Hello' />
         <Button func={this.world} btnName='World' />
         <Button func={this.exclamations} btnName='!!!' />
+        <Button func={this.counter} btnName={count} btnStyle={color} />
       </div>
     )
   }
--- trybe-exercicios/frontEnd/secao03ComponentesComEstadoEventosEFormulariosComReact/dia01ComponentesComEstadoEEventos/fancy-buttons/src/components/Button.js
+++ trybe-exercicios/frontEnd/secao03ComponentesComEstadoEventosEFormulariosComReact/dia01ComponentesComEstadoEEventos/fancy-buttons/src/components/Button.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Button = ({func, btnName}) => <button onClick={ func }>{ btnName }</button>;
+const Button = ({func, btnName, btnStyle}) => <button onClick={ func } style={ {backgroundColor: btnStyle} }>{ btnName }</button>;
 
 Button.propTypes = {
   func:PropTypes.func,
-  btnName: PropTypes.string
+  btnName: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  btnStyle: PropTypes.string
 }
 
 export default Button
