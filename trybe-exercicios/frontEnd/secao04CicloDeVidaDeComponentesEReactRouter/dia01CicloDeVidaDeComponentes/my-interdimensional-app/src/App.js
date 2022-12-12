da05e5f58282fa68d848f8125d14c195e7adc34c
KennyKoyama <kennysk1982@gmail.com>
2022-12-12 13:27:35 -0300

    Refactoring

--- trybe-exercicios/frontEnd/secao04CicloDeVidaDeComponentesEReactRouter/dia01CicloDeVidaDeComponentes/my-interdimensional-app/src/App.js
+++ trybe-exercicios/frontEnd/secao04CicloDeVidaDeComponentesEReactRouter/dia01CicloDeVidaDeComponentes/my-interdimensional-app/src/App.js
@@ -8,48 +8,18 @@ class App extends Component {
     super(props);
     this.state = {
         characters: [],
-        jokeObj: undefined,
-        loading: true,
-        storedJokes: [],
     };
   }
 
   updateState = (key, value, second) => this.setState({ [key]: value });
 
-  async fetchJoke() {
-    this.setState(
-      {loading: true},
-      async () => {
-        const requestHeaders = { headers: { Accept: 'application/json' } }
-        const requestReturn = await fetch('https://icanhazdadjoke.com/', requestHeaders)
-        const requestObject = await requestReturn.json();
-        this.setState({
-          loading: false,
-          jokeObj: requestObject
-        })
-      })
-  }
 
-  saveJoke = () => {
-    this.setState(({ storedJokes, jokeObj }) => (
-      { storedJokes: [...storedJokes, jokeObj]}
-    ))
-    this.fetchJoke()
-  };
-
-  componentDidMount() {
-    this.fetchJoke()
-  }
 
   render() {
     const { characters, jokeObj, loading, storedJokes } = this.state;
     return (
       <div className="App">
-        <DadJoke
-          dadJokes={ {jokeObj, loading, storedJokes} }
-          fetchJoke={ this.fetchJoke }
-          saveJoke= { this.saveJoke }
-        />
+        <DadJoke />
         <RickMorty
           characters={ characters }
           updateState={ this.updateState }
--- trybe-exercicios/frontEnd/secao04CicloDeVidaDeComponentesEReactRouter/dia01CicloDeVidaDeComponentes/my-interdimensional-app/src/components/DadJoke.js
+++ trybe-exercicios/frontEnd/secao04CicloDeVidaDeComponentesEReactRouter/dia01CicloDeVidaDeComponentes/my-interdimensional-app/src/components/DadJoke.js
@@ -2,27 +2,40 @@ import React from 'react';
 import Joke from './Joke';
 
 class DadJoke extends React.Component {
-  // async fetchJoke() {
-  //   const { updateState } = this.props;
-  //   const newFetch = async () => {
-  //     const requestHeaders = { headers: { Accept: 'application/json' } }
-  //     const requestReturn = await fetch('https://icanhazdadjoke.com/', requestHeaders)
-  //     const requestObject = await requestReturn.json();
-  //     updateState('loading', false)
-  //     updateState('jokeObj', requestObject);
-  //   }
-  //   updateState('loading', true, newFetch)
-  // }
+  state = {
+    jokeObj: undefined,
+    loading: true,
+    storedJokes: [],
+  }
   
-  // componentDidMount() {
-  //   const { fetchJoke } = this.props;
-  //   fetchJoke();
-  // }
+  async fetchJoke() {
+    this.setState(
+      {loading: true},
+      async () => {
+        const requestHeaders = { headers: { Accept: 'application/json' } }
+        const requestReturn = await fetch('https://icanhazdadjoke.com/', requestHeaders)
+        const requestObject = await requestReturn.json();
+        this.setState({
+          loading: false,
+          jokeObj: requestObject
+        })
+      })
+  }
+
+  saveJoke = () => {
+    this.setState(({ storedJokes, jokeObj }) => (
+      { storedJokes: [...storedJokes, jokeObj]}
+    ))
+    this.fetchJoke()
+  };
+
+  componentDidMount() {
+    this.fetchJoke()
+  }
 
   render() {
-    const { dadJokes: { jokeObj, loading, storedJokes }, saveJoke } = this.props;
+    const { jokeObj, loading, storedJokes } = this.state;
     const loadingElement = <span>Loading...</span>;
-    // console.log('STORED JOKES', storedJokes)
 
     return (
       <div>
@@ -39,7 +52,7 @@ class DadJoke extends React.Component {
 
         <p>RENDERIZAÇÃO CONDICIONAL</p>
         */
-        loading ? loadingElement : <Joke jokeObj={jokeObj} saveJoke={saveJoke} />
+        loading ? loadingElement : <Joke jokeObj={jokeObj} saveJoke={this.saveJoke} />
       }
 
       </div>
