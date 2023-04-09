75b44548130c9662cdf862a21892b05189a62a6e
KennyKoyama <kennysk1982@gmail.com>
2023-04-09 18:24:39 -0300

    Exercise 4 Done

--- trybe-exercicios/backEnd/secao04IntroducaoAoDesenvolvimentoWebComNodeJs/dia01NodeJsRuntimeAssincrono/my-scripts/bmi.js
+++ trybe-exercicios/backEnd/secao04IntroducaoAoDesenvolvimentoWebComNodeJs/dia01NodeJsRuntimeAssincrono/my-scripts/bmi.js
@@ -1,6 +1,6 @@
 const readline = require('readline-sync');
-const weightInput = readline.questionInt("What’s your weight?");
-const heightInput = readline.questionFloat("What’s your height?");
+const weightInput = readline.questionFloat("What’s your weight? ");
+const heightInput = readline.questionFloat("What’s your height? ");
 
 function bmi(weight, height) {
   return weight / (height ** 2);
